a3842c4b4d5c07a27b22a85103bb46c99c4c1e42
1837669410 <1837669410@qq.com>
2026-01-26 20:06:37 +0800

    update information

--- nanotrainer/__init__.py
+++ nanotrainer/__init__.py
@@ -4,7 +4,10 @@ __license__ = "Apache-2.0"
 __description__ = "A naon experimental trainer for Pytorch"
 
 # callback
-from .callback import logging, checkpoint
+from .callback import checkpoint, default, logging
+
+# experiment
+from .experiment import config, cv
 
 # state
 
