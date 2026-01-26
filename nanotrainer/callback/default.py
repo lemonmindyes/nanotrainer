f3ee21e356b7934d23562a5e3800ed357e0557b5
1837669410 <1837669410@qq.com>
2026-01-26 20:05:42 +0800

    add default callbacks

---
+++ nanotrainer/callback/default.py
@@ -0,0 +1,15 @@
+from .logging import ModelSummaryCallback, LoggingCallback
+from .checkpoint import CheckpointCallback
+
+
+def get_default_callbacks(save_path: str,
+                          save_interval: int = 1000,
+                          auto_recover: str = None,
+                          recover_path: str = None,
+                          log_interval: int = 100
+                          ):
+    return [
+        ModelSummaryCallback(),
+        LoggingCallback(log_interval),
+        CheckpointCallback(save_path, save_interval, auto_recover, recover_path),
+    ]
